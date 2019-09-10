3647134e14383ae110804e7805f248a5ce5f704a
Joe Soboleski <soboleskijoe@gmail.com>
2019-09-09 19:19:19 -0700

    Version 0.1.0 Basic App Stuff

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Home'),
+                IconButton(
+                  icon: Icon(Icons.settings),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()),
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
